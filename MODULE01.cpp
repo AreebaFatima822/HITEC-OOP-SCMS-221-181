b53504f43fc07d64dbdfffe359da0dfa7447fac3
RidaSajid1122 <25-CS-061@student.hitecuni.edu.pk>
2026-06-21 13:22:27 +0500

    Create MODULE01.cpp

---
+++ MODULE01.cpp
@@ -0,0 +1,181 @@
+#include<iostream>
+using namespace std;
+
+class person
+{
+    protected:
+    string name;
+    int CNIC;
+    int age;
+    string contact;
+    public:
+    virtual void displayinfo() = 0;
+    person(string a,int b,int c,string d)
+    {
+        name = a;
+        CNIC = b;
+        age = c;
+        contact = d;
+    }
+    void input()
+    {
+        cout<<"Enter Name : "<<endl;
+        cin>>name;
+        cout<<"Enter CNIC : "<<endl;
+        cin>>CNIC;
+        cout<<"Enter Age : "<<endl;
+        cin>>age;
+        cout<<"Enter Contact Number : "<<endl;
+        cin>>contact;
+    }
+};
+class student : public person
+{
+    private:
+    string rollno;
+    int semester;
+    float gpa;
+    int encourses[5];
+    public:
+    student(string a,int b,int c,string d,string e,int f,float g,int h) : person(a,b,c,d)
+    {
+        rollno = e;
+        semester = f;
+        gpa = g;
+        encourses [0] =h;
+    }
+    void input()
+    {
+        cout<<"Enter Roll Number : "<<endl;
+        cin>>rollno;
+        cout<<"Enter Semester : "<<endl;
+        cin>>semester;
+        cout<<"Enter GPA : "<<endl;
+        cin>>gpa;
+        cout<<"Enter enrollment courses : "<<endl;
+        for(int i=0;i<=5;i++)
+        {
+            cin>>encourses[i];
+        }
+        person::input();
+    }
+    void displayinfo()
+    {
+        cout<<"Roll Number : "<<rollno<<endl;
+        cout<<"Semester : "<<semester<<endl;
+        cout<<"GPA : "<<gpa<<endl;
+        cout<<"Enrollment Courses"<<endl;
+        for(int i=0;i<=5;i++)
+        {
+            cout<<encourses[i]<<endl;;
+        }
+    }
+    void calculategrade()
+    {
+        if(gpa > 3.5)
+        {
+            cout<<"A Grade."<<endl;
+        }
+        else if(gpa > 3 && gpa <3.5)
+        {
+            cout<<"B Grade."<<endl;
+        }
+        else if (gpa > 2.5 && gpa <3)
+        {
+            cout<<"C Grade."<<endl;
+        }
+        else if (gpa > 2 && gpa <2.5)
+        {
+            cout<<"D Grade."<<endl;
+        } 
+        else
+        {
+            cout<<"F Grade."<<endl;
+        }       
+    }
+};
+class faculity : public person
+{
+    private:
+    int empid;
+    string department;
+    string designation;
+    int asscourses;
+    public:
+    faculity(string a,int b,int c,string d,int e,string f,string g,int h) : person(a,b,c,d)
+    {
+        empid=e;
+        department=f;
+        designation=g;
+        asscourses=h;
+    }
+    void input()
+    {
+        cout<<"Enter Employee ID : "<<endl;
+        cin>>empid;
+        cout<<"Enter Department : "<<endl;
+        cin>>department;
+        cout<<"Enter Designation : "<<endl;
+        cin>>designation;
+        cout<<"Enter Number of Assigned Courses : "<<endl;
+        cin>>asscourses;
+        person::input();
+    }
+    void displayinfo()
+    {
+        cout<<"Employee ID : "<<empid<<endl;
+        cout<<"Department : "<<department<<endl;
+        cout<<"Designation : "<<designation<<endl;
+        cout<<"Assigned Courses : "<<asscourses<<endl;
+    }
+};
+class staff : public person
+{
+    private:
+    int staffid;
+    string role;
+    float salary;
+    public:
+    staff(string a,int b,int c,string d,int e,string f,float g) : person(a,b,c,d)
+    {
+        staffid = e;
+        role = f;
+        salary = g;
+    }
+    void input()
+    {
+        cout<<"Enter Staff ID : "<<endl;
+        cin>>staffid;
+        cout<<"Enter Role : "<<endl;
+        cin>>role;
+        cout<<"Enter Salary : "<<endl;
+        cin>>salary;
+        person::input();
+    }
+    void displayinfo()
+    {
+        cout<<"Staff ID : "<<staffid<<endl;
+        cout<<"Role : "<<role<<endl;
+        cout<<"Salary : "<<salary<<endl;
+    }
+};
+int main()
+{
+    person *P1 = new student("Rida",36104,19,"0317745","061",2,3.8,4);
+    person *P2 = new faculity("Hajra",45267,15,"0313245",157,"computer science","Lecturer",2);
+    person *P3 = new staff("Fatima",56791,23,"0315234",121,"Assistant",40000);
+
+    P1->input();
+    P1->displayinfo();
+    cout<<endl;
+
+    P2->input();
+    P2->displayinfo();
+    cout<<endl;
+
+    P3->input();
+    P3->displayinfo();
+
+    return 0;
+
+}
