44f150f492bcd64ff934608e3d89ba52dfe24829
AreebaFatima822 <25-cs-221@student.hitecuni.edu.pk>
2026-06-21 21:51:17 +0500

    Update Module-5.cpp

--- Module-5.cpp
+++ Module-5.cpp
@@ -1,248 +1,15 @@
-
 #include <iostream>
 #include <string>
-
 using namespace std;
 
-class Student
+class HostelStudent
 {
 private:
     int studentID;
     string name;
 
 public:
-    Student(int id = 0, string n = "")
-    {
-        studentID = id;
-        name = n;
-    }
-
-    string getName()
-    {
-        return name;
-    }
-};
-
-class Person
-{
-public:
-    virtual ~Person() {}
-};
-
-class Accommodation : virtual public Person
-{
-public:
-    virtual void allocateRoom(Student* student, int roomNo) = 0;
-    virtual void vacateRoom(int roomNo, Student* student) = 0;
-};
-
-class Reportable : virtual public Person
-{
-public:
-    virtual void generateReport() = 0;
-};
-
-class Room
-{
-private:
-    int roomNumber;
-    string type;
-    int floor;
-    Student* occupants[3];
-    int occupantCount;
-
-public:
-    Room(int rn = 0, string t = "Single", int f = 0)
-    {
-        roomNumber = rn;
-        type = t;
-        floor = f;
-        occupantCount = 0;
-
-        for (int i = 0; i < 3; i++)
-            occupants[i] = nullptr;
-    }
-
-    int getRoomNumber()
-    {
-        return roomNumber;
-    }
-
-    int getCapacity()
-    {
-        if (type == "Single")
-            return 1;
-        if (type == "Double")
-            return 2;
-        return 3;
-    }
-
-    bool addOccupant(Student* s)
-    {
-        if (occupantCount >= getCapacity())
-            return false;
-
-        occupants[occupantCount++] = s;
-        return true;
-    }
-
-    bool removeOccupant(Student* s)
-    {
-        for (int i = 0; i < occupantCount; i++)
-        {
-            if (occupants[i] == s)
-            {
-                for (int j = i; j < occupantCount - 1; j++)
-                    occupants[j] = occupants[j + 1];
-
-                occupants[occupantCount - 1] = nullptr;
-                occupantCount--;
-                return true;
-            }
-        }
-        return false;
-    }
-
-    void display()
-    {
-        cout << "Room: " << roomNumber
-             << " Type: " << type
-             << " Floor: " << floor
-             << " Occupants: " << occupantCount << "/"
-             << getCapacity() << endl;
-
-        for (int i = 0; i < occupantCount; i++)
-            cout << "   - " << occupants[i]->getName() << endl;
-    }
-};
-
-class HostelBlock
-{
-private:
-    string blockName;
-    Room rooms[5];
-
-public:
-    HostelBlock(string name = "")
-    {
-        blockName = name;
-
-        rooms[0] = Room(101, "Single", 1);
-        rooms[1] = Room(102, "Double", 1);
-        rooms[2] = Room(201, "Triple", 2);
-        rooms[3] = Room(202, "Double", 2);
-        rooms[4] = Room(301, "Single", 3);
-    }
-
-    Room* getRoom(int roomNo)
-    {
-        for (int i = 0; i < 5; i++)
-        {
-            if (rooms[i].getRoomNumber() == roomNo)
-                return &rooms[i];
-        }
-        return nullptr;
-    }
-
-    void displayRooms()
-    {
-        cout << "\nBlock: " << blockName << endl;
-
-        for (int i = 0; i < 5; i++)
-            rooms[i].display();
-    }
-};
-
-class HostelManager : public Accommodation, public Reportable
-{
-private:
-    HostelBlock block;
-
-public:
-    HostelManager(string blockName) : block(blockName)
-    {
-    }
-
-    void allocateRoom(Student* student, int roomNo)
-    {
-        Room* room = block.getRoom(roomNo);
-
-        if (room == nullptr)
-        {
-            cout << "Room not found.\n";
-            return;
-        }
-
-        if (room->addOccupant(student))
-            cout << student->getName()
-                 << " allocated to Room "
-                 << roomNo << endl;
-        else
-            cout << "Room is full.\n";
-    }
-
-    void vacateRoom(int roomNo, Student* student)
-    {
-        Room* room = block.getRoom(roomNo);
-
-        if (room == nullptr)
-        {
-            cout << "Room not found.\n";
-            return;
-        }
-
-        if (room->removeOccupant(student))
-            cout << student->getName()
-                 << " vacated Room "
-                 << roomNo << endl;
-        else
-            cout << "Student not found in room.\n";
-    }
-
-    void generateReport()
-    {
-        cout << "\nHostel Occupancy Report\n";
-        block.displayRooms();
-    }
-};
-
-int main()
-{
-    Student s1(1, "Fatima");
-    Student s2(2, "Hafsa");
-    Student s3(3, "Zara");
-    Student s4(4, "Rida");
-
-    HostelManager manager("A Block");
-
-    manager.allocateRoom(&s1, 102);
-    manager.allocateRoom(&s2, 102);
-    manager.allocateRoom(&s3, 102);
-
-    manager.allocateRoom(&s4, 201);
-
-    manager.generateReport();
-
-    manager.vacateRoom(102, &s1);
-
-    manager.generateReport();
-
-    return 0;
-}
-
-#include <iostream>
-#include <string>
-
-using namespace std;
-
-class Student
-{
-private:
-    int studentID;
-    string name;
-
-public:
-    Student(int id = 0, string n = "")
+    HostelStudent(int id = 0, string n = "")
     {
         studentID = id;
         name = n;
@@ -259,20 +26,20 @@ public:
     }
 };
 
-class Person
+class HostelBase
 {
 public:
-    virtual ~Person() {}
+    virtual ~HostelBase() {}
 };
 
-class Accommodation : virtual public Person
+class Accommodation : virtual public HostelBase
 {
 public:
-    virtual void allocateRoom(Student *student, int roomNo) = 0;
-    virtual void vacateRoom(int roomNo, Student *student) = 0;
+    virtual void allocateRoom(HostelStudent *student, int roomNo) = 0;
+    virtual void vacateRoom(int roomNo, HostelStudent *student) = 0;
 };
 
-class Reportable : virtual public Person
+class Reportable : virtual public HostelBase
 {
 public:
     virtual void generateReport() = 0;
@@ -284,7 +51,7 @@ private:
     int roomNumber;
     string type;
     int floor;
-    Student *occupants[3];
+    HostelStudent *occupants[3];
     int occupantCount;
 
 public:
@@ -296,9 +63,7 @@ public:
         occupantCount = 0;
 
         for (int i = 0; i < 3; i++)
-        {
             occupants[i] = NULL;
-        }
     }
 
     int getRoomNumber()
@@ -316,34 +81,30 @@ public:
             return 3;
     }
 
-    bool addOccupant(Student *s)
+    bool addOccupant(HostelStudent *s)
     {
         if (occupantCount >= getCapacity())
             return false;
 
         occupants[occupantCount] = s;
         occupantCount++;
-
         return true;
     }
 
-    bool removeOccupant(Student *s)
+    bool removeOccupant(HostelStudent *s)
     {
         for (int i = 0; i < occupantCount; i++)
         {
             if (occupants[i] == s)
             {
                 for (int j = i; j < occupantCount - 1; j++)
-                {
                     occupants[j] = occupants[j + 1];
-                }
 
                 occupants[occupantCount - 1] = NULL;
                 occupantCount--;
                 return true;
             }
         }
-
         return false;
     }
 
@@ -388,9 +149,7 @@ public:
         for (int i = 0; i < 5; i++)
         {
             if (rooms[i].getRoomNumber() == roomNo)
-            {
                 return &rooms[i];
-            }
         }
 
         return NULL;
@@ -401,9 +160,7 @@ public:
         cout << "\nBlock Name: " << blockName << endl;
 
         for (int i = 0; i < 5; i++)
-        {
             rooms[i].display();
-        }
     }
 };
 
@@ -418,7 +175,7 @@ public:
     {
     }
 
-    void allocateRoom(Student *student, int roomNo)
+    void allocateRoom(HostelStudent *student, int roomNo)
     {
         Room *room = block.getRoom(roomNo);
 
@@ -440,7 +197,7 @@ public:
         }
     }
 
-    void vacateRoom(int roomNo, Student *student)
+    void vacateRoom(int roomNo, HostelStudent *student)
     {
         Room *room = block.getRoom(roomNo);
 
@@ -464,18 +221,17 @@ public:
 
     void generateReport()
     {
-        cout << "     HOSTEL REPORT     ";
+        cout << "\n===== HOSTEL REPORT =====\n";
         block.displayRooms();
-      
     }
 };
 
 int main()
 {
-    Student s1(1, "Fatima");
-    Student s2(2, "Hafsa");
-    Student s3(3, "Zara");
-    Student s4(4, "Rida");
+    HostelStudent s1(1, "Fatima");
+    HostelStudent s2(2, "Hafsa");
+    HostelStudent s3(3, "Zara");
+    HostelStudent s4(4, "Rida");
 
     HostelManager manager("A Block");
 
@@ -488,8 +244,9 @@ int main()
 
     manager.vacateRoom(102, &s1);
 
+    cout << "\nAfter Vacating:\n";
+
     manager.generateReport();
 
     return 0;
 }
-
